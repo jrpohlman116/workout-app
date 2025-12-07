de20b59aa6eb0db6195f1767bde183768e10ea29
Jordan Martinuzzi <jordanpohlman@gmail.com>
2025-12-06 21:01:01 -0700

    Updated AccessibleProgressRing.tsx

--- src/components/accessible/AccessibleProgressRing.tsx
+++ src/components/accessible/AccessibleProgressRing.tsx
@@ -85,9 +85,6 @@ export default function AccessibleProgressRing({
               <div className="text-sm text-gray-900 dark:text-gray-100 mt-1" aria-hidden="true">
                 {description}
               </div>
-              <div className="text-xs text-gray-700 dark:text-gray-100 mt-1" aria-hidden="true">
-                (Projected)
-              </div>
             </>)}
           </div>
         </div>
